f2333b749cc7724d382f8616dbdcf4e1a9d91f13
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-23 16:41:26 -0300

    Criar arquivo de listagem de elementos da página

---
+++ cypress/support/elements/LoginElements.js
@@ -0,0 +1,15 @@
+class LoginElements {
+  inserirNomeUsuario = () => {
+    return "#user-name";
+  };
+
+  inserirSenhaUsuario = () => {
+    return "#password";
+  };
+
+  clicarBotaoLogin = () => {
+    return "#login-button";
+  };
+}
+
+export default LoginElements;
