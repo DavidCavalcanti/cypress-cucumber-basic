cf1f91a86e3239c425c458d3a749a4a2305344c2
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-23 16:36:40 -0300

    Configurações  iniciais

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,14 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
+
+module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www.saucedemo.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("file:preprocessor", cucumber());
     },
+    defaultCommandTimeout: 10000,
+    chromeWebSecurity: false
   },
-};
+});
